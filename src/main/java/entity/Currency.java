b93ef3bc83a0b1580705a6b1b4dc1504e37a0ab9
Dmitriy Butenko <dimas.ukimas@gmail.com>
2024-09-16 19:49:11 +0300

    refactor(entity): remove Builder pattern from Currency class
    Initially Buider pattern was added in Currency class for study purpose.
    But lately it became a redundant obstacle due to lack of flexibility.

--- src/main/java/entity/Currency.java
+++ src/main/java/entity/Currency.java
@@ -2,17 +2,27 @@ package entity;
 
 public class Currency {
 
-    private final int id;
-    private final String name;
-    private final String code;
-    private final String sign;
+    private int id;
+    private String name;
+    private String code;
+    private String sign;
 
-    private Currency(CurrencyBuilder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.code = builder.code;
-        this.sign = builder.sign;
+    public void setId(int id) {
+        this.id = id;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
 
     public int getId() {
         return id;
@@ -30,45 +40,45 @@ public class Currency {
         return sign;
     }
 
-    public static class CurrencyBuilder {
-        private int id;
-        private String name;
-        private String code;
-        private String sign;
-
-        public CurrencyBuilder setId(int id) {
-            this.id = id;
-            return this;
-        }
-
-        public CurrencyBuilder setName(String name) {
-            this.name = name;
-            return this;
-
-        }
-
-        public CurrencyBuilder setCode(String code) {
-            this.code = code;
-            return this;
-        }
-
-        public CurrencyBuilder setSign(String sign) {
-            this.sign = sign;
-            return this;
-        }
-
-        public Currency build() {
-            return new Currency(this);
-        }
-    }
+//    public static class CurrencyBuilder {
+//        private int id;
+//        private String name;
+//        private String code;
+//        private String sign;
+//
+//        public CurrencyBuilder setId(int id) {
+//            this.id = id;
+//            return this;
+//        }
+//
+//        public CurrencyBuilder setName(String name) {
+//            this.name = name;
+//            return this;
+//
+//        }
+//
+//        public CurrencyBuilder setCode(String code) {
+//            this.code = code;
+//            return this;
+//        }
+//
+//        public CurrencyBuilder setSign(String sign) {
+//            this.sign = sign;
+//            return this;
+//        }
+//
+//        public Currency build() {
+//            return new Currency(this);
+//        }
+//    }
 
     @Override
-    public String toString(){
-        return "Currency{"+
-                "id="+id+
-                ", name="+name+
-                ", code="+code+
-                ", sign="+sign+
+    public String toString() {
+        return "Currency{" +
+                "id=" + id +
+                ", name=" + name +
+                ", code=" + code +
+                ", sign=" + sign +
                 "}";
 
     }
