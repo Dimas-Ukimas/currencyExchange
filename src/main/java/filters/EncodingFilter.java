d7728f723de8d0248141feee7793e7b821c508ed
Dmitriy Butenko <dimas.ukimas@gmail.com>
2024-10-24 17:38:32 +0300

    feat: add CORS support in EncodingFilter class

--- src/main/java/filters/EncodingFilter.java
+++ src/main/java/filters/EncodingFilter.java
@@ -2,6 +2,8 @@ package filters;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import util.PropertiesUtil;
 
 import java.io.IOException;
 
@@ -11,9 +13,16 @@ public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
         servletRequest.setCharacterEncoding("UTF-8");
         servletResponse.setCharacterEncoding("UTF-8");
         servletResponse.setContentType("application/json");
+        httpResponse.setHeader("Access-Control-Allow-Origin", PropertiesUtil.get("api.ip"));
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        httpResponse.setHeader("Access-Control-Expose-Headers", "Authorization");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
